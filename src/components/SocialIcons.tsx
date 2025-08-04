0201ef3e8fb905a246ad88475359ae2f27d51449
crisparisi-MB <cris@midnightbrunch.com>
2025-08-04 17:26:33 -0400

    Update SocialIcons.tsx

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const SocialIcons: React.FC = () => {
   return (
-    <div className="flex space-x-6">
+    <div className="flex space-x-3 sm:space-x-4 md:space-x-6">
       {/* LinkedIn */}
       <a
         href="http://www.linkedin.com/company/prometrika-llc"
         target="_blank"
         rel="noopener noreferrer"
-        className="transition-all duration-300 hover:scale-110 group"
+        className="transition-all duration-300 hover:scale-110 group touch-manipulation"
         aria-label="LinkedIn"
       >
         <img 
           src="/linkedin-in.svg" 
          alt="Follow us on LinkedIn" 
-          className="w-7 h-7 object-contain filter brightness-0 invert group-hover:brightness-100 group-hover:invert-0 transition-all duration-300"
+          className="w-5 sm:w-6 md:w-7 h-5 sm:h-6 md:h-7 object-contain filter brightness-0 invert group-hover:brightness-100 group-hover:invert-0 transition-all duration-300"
           style={{
             filter: 'brightness(0) invert(1)'
           }}
@@ -26,13 +26,13 @@ const SocialIcons: React.FC = () => {
         href="https://twitter.com/PrometrikaCRO"
         target="_blank"
         rel="noopener noreferrer"
-        className="transition-all duration-300 hover:scale-110 group"
+        className="transition-all duration-300 hover:scale-110 group touch-manipulation"
         aria-label="X (Twitter)"
       >
         <img 
           src="/x-twitter.svg" 
          alt="Follow us on X (Twitter)" 
-          className="w-7 h-7 object-contain filter brightness-0 invert group-hover:brightness-100 group-hover:invert-0 transition-all duration-300"
+          className="w-5 sm:w-6 md:w-7 h-5 sm:h-6 md:h-7 object-contain filter brightness-0 invert group-hover:brightness-100 group-hover:invert-0 transition-all duration-300"
           style={{
             filter: 'brightness(0) invert(1)'
           }}
@@ -44,13 +44,13 @@ const SocialIcons: React.FC = () => {
         href="https://www.facebook.com/prometrikallc"
         target="_blank"
         rel="noopener noreferrer"
-        className="transition-all duration-300 hover:scale-110 group"
+        className="transition-all duration-300 hover:scale-110 group touch-manipulation"
         aria-label="Facebook"
       >
         <img 
           src="/facebook-f.svg" 
          alt="Follow us on Facebook" 
-          className="w-7 h-7 object-contain filter brightness-0 invert group-hover:brightness-100 group-hover:invert-0 transition-all duration-300"
+          className="w-5 sm:w-6 md:w-7 h-5 sm:h-6 md:h-7 object-contain filter brightness-0 invert group-hover:brightness-100 group-hover:invert-0 transition-all duration-300"
           style={{
             filter: 'brightness(0) invert(1)'
           }}
