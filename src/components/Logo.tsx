bd2fd97542f5e5de37b2d5eb27266ef363cfd05b
crisparisi-MB <cris@midnightbrunch.com>
2025-08-04 17:26:19 -0400

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ const Logo: React.FC = () => {
   return (
     <a 
       href="/" 
-      className="block w-84 md:w-96 lg:w-110 h-20 md:h-24 lg:h-28 transition-transform duration-300 hover:scale-105"
+      className="block w-20 sm:w-24 md:w-32 lg:w-72 xl:w-80 h-12 sm:h-16 md:h-20 lg:h-44 xl:h-48 transition-transform duration-300 hover:scale-105"
       aria-label="Go to homepage"
     >
       <img
